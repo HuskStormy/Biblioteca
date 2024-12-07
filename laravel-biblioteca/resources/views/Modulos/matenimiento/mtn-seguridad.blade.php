a260a0bfcb37e3b9778d48acfe94ff67fbf29708
HuskStormy <catstormy9@gmail.com>
2024-12-06 20:47:27 -0600

    .,

--- laravel-biblioteca/resources/views/Modulos/matenimiento/mtn-seguridad.blade.php
+++ laravel-biblioteca/resources/views/Modulos/matenimiento/mtn-seguridad.blade.php
@@ -16,7 +16,7 @@
                     <!-- Botones alineados al lado derecho -->
                     <div class="d-flex gap-2 ms-auto">
                         <!-- Botón Agregar -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal1">
                             <i class="bi bi-plus-circle"></i> Agregar
                         </button>
 
@@ -44,7 +44,7 @@
                                 <td>descripcion</td>
                                 <td>
                                     <div class="button-group">
-                                        <button type="button" class="btn_editar" data-bs-toggle="modal" data-bs-target="#editModal">
+                                        <button type="button" class="btn_editar" data-bs-toggle="modal" data-bs-target="#editModal1">
                                             <i class="bi bi-pen-fill">     Editar  </i>
                                         </button>
                                         <button type="button" class="btn_eliminar" data-bs-toggle="modal" data-bs-target="#deleteModal">
@@ -78,7 +78,7 @@
                         <!-- Botones alineados al lado derecho -->
                         <div class="d-flex gap-2 ms-auto">
                             <!-- Botón Agregar -->
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal2">
                                 <i class="bi bi-plus-circle"></i> Agregar
                             </button>
 
@@ -106,7 +106,7 @@
                                     <td>descripcion</td>
                                     <td>
                                         <div class="button-group">
-                                            <button type="button" class="btn_editar" data-bs-toggle="modal" data-bs-target="#editModal">
+                                            <button type="button" class="btn_editar" data-bs-toggle="modal" data-bs-target="#editModal2">
                                                 <i class="bi bi-pen-fill">     Editar  </i>
                                             </button>
                                             <button type="button" class="btn_eliminar" data-bs-toggle="modal" data-bs-target="#deleteModal">
@@ -153,5 +153,200 @@
     </div>
 </div>
 
+    <!-- Modal para Agregar -->
+    <div class="modal fade" id="addModal1" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">Crear Estado de usuario</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="row">
+
+                                <div class="col-12">
+                                    <div class="row">
+                                        <label for="titulo" class="form-label">Datos</label>
+                                        <div class="col-6">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend"> <span class="input-group-text">ID</span></div>
+                                                <input type="text" class="form-control" placeholder="Texto">
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend"> <span class="input-group-text">Estado</span></div>
+                                                <input type="text" class="form-control" placeholder="Texto">
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend"> <span class="input-group-text">Descripcion</span></div>
+                                                <textarea class="form-control" placeholder="" maxlength="255" rows="5"></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <!-- Modal para Editar -->
+    <div class="modal fade" id="editModal1" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Editar Estado de usuario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="row">
+
+                            <div class="col-12">
+                                <div class="row">
+                                    <label for="titulo" class="form-label">Datos</label>
+                                    <div class="col-6">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend"> <span class="input-group-text">ID</span></div>
+                                            <input type="text" class="form-control" placeholder="Texto">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend"> <span class="input-group-text">Estado</span></div>
+                                            <input type="text" class="form-control" placeholder="Texto">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend"> <span class="input-group-text">Descripcion</span></div>
+                                            <textarea class="form-control" placeholder="" maxlength="255" rows="5"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!-- Modal para Agregar -->
+    <div class="modal fade" id="addModal2" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">Crear Tipo de Objeto</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="row">
+
+                                <div class="col-12">
+                                    <div class="row">
+                                        <label for="titulo" class="form-label">Datos</label>
+                                        <div class="col-6">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend"> <span class="input-group-text">ID</span></div>
+                                                <input type="text" class="form-control" placeholder="Texto">
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend"> <span class="input-group-text">Tipo</span></div>
+                                                <input type="text" class="form-control" placeholder="Texto">
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend"> <span class="input-group-text">Descripcion</span></div>
+                                                <textarea class="form-control" placeholder="" maxlength="255" rows="5"></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <!-- Modal para Editar -->
+    <div class="modal fade" id="editModal2" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Editar Tipo de Objeto</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="row">
+
+                            <div class="col-12">
+                                <div class="row">
+                                    <label for="titulo" class="form-label">Datos</label>
+                                    <div class="col-6">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend"> <span class="input-group-text">ID</span></div>
+                                            <input type="text" class="form-control" placeholder="Texto">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend"> <span class="input-group-text">Tipo</span></div>
+                                            <input type="text" class="form-control" placeholder="Texto">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend"> <span class="input-group-text">Descripcion</span></div>
+                                            <textarea class="form-control" placeholder="" maxlength="255" rows="5"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
